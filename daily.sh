335543a43986566abf952a39e4a4261039bc38ca
RekGRpth <rekgrpth@gmail.com>
2018-04-23 09:47:21 +0500

    -daily

--- daily.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-find applications -type d -maxdepth 1 -mindepth 1 | grep -v "__pycache__" | while read -r APP; do
-    APP="$(basename "$APP")"
-    python3 web2py.py -Q -S "$APP" -M -R scripts/sessions2trash.py -A -o
-    python3 web2py.py -Q -S "$APP" -R scripts/zip_static_files.py
-done
